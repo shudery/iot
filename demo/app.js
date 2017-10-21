cb48621b0b60ed6f255d2fc4559107c3024aaab0
daguo <shudery@foxmail.com>
2017-10-22 02:57:03 +0800

    add app tweet

--- demo/app.js
+++ demo/app.js
@@ -1,42 +1,66 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+var expressWs = require('express-ws')(app);  
+var n = 0;
+// var iotRouterUrl = 'http://192.168.43.118:8080';
+var iotRouterUrl = 'http://192.168.31.232:8080';
 
-var eventBus = {
-    //TODO
-}
+var websocket;
 
-var iotRouterUrl = '';
-var serRouterMap = {
-    
-}
+//设置跨域访问  
+app.all('*', function(req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+    res.header("X-Powered-By",' 3.2.1')  
+    res.header("Content-Type", "application/json;charset=utf-8");  
+    next();  
+});  
 
-app.get('/uploadDate',()=>{
-
-    //存数据库
-    DB.store();
-    //触发订阅事件，通知APP，渲染UI
-    eventBus.trigger()
+app.ws('/ws',(ws,res)=>{
+    log('[app->server]strat ws connection.');
+    ws.on('message',msg=>{
+        log('[app->server]'+msg)
+        ws.send('conneted');
+    })
+    websocket = ws;
 });
 
 //应用端直接请求网关服务即可
-app.get('/onWarnLED',()=>{
-    http.get(iotRouterUrl + '/onWarnLED',(res)=>{
-
+app.get('/changeWarnLED',(req,res)=>{
+    log('[app->server]changeWarnLED')
+    http.get(iotRouterUrl + '/changeWarnLED',(resp)=>{
+        res.send('[server->app]ok!')
+    }).on('error',e=>{
+        log(e);
     });
 
 });
 
 //接收设备的告警信息
-app.get('/',(req, res)=>{
-    console.log(new Date() +':somebody have a action now.');
-    res.send('The warn msg send success.');
+app.get('/warnSignal',(req, res)=>{
+    log('[rpio->server]somebody have a action now.');
+    res.send('[server->rpio]The warn msg send success.');
     //存储信息
-    
-    //发布消息到APP
+    var store = {
+        timeLabel : Date.now(),
+        msgType : 'warnSignal',
+    }
 
+    //发布消息到APP
+    // try{
+    //     websocket.send('[server->app]warn Signal.')
+
+    // }catch(e){
+    //     log('please start app function of websocket.')
+    // }
 })
 
 app.listen(8066,()=>{
-    console.log('start server.')
+    log('start server at 8066.')
 })
+
+function log(str) {
+    console.log('[' + Date.now() + ']' + str);
+}
--- demo/server.js
+++ demo/server.js
@@ -2,10 +2,13 @@
  * IOT编程：活动感应器
  * 1：监听传感器输出，人体活动时告警灯闪烁，并上传消息
  * 2：设备远程控制：告警灯状态，告警功能是否开启
-**/
+ **/
 var server = require('server');
 var { get } = server.router;
+var https = require('https');
 var http = require('http');
+var exec = require('child_process').exec; 
+var request = require('request');
 var rpio = require('rpio');
 
 //监听引脚
@@ -15,7 +18,9 @@ const WARNLED_PIN = 12;
 //设备服务端口
 const SERVER_PORT = 8080;
 //消息上传地址
-const CONSOLE_URL = 'http://192.168.43.10:8066';
+// const CONSOLE_URL = 'http://192.168.43.10:8066';
+const CONSOLE_URL = 'http://192.168.31.170:8066';
+const WEB_HOCK = 'https://hook.bearychat.com/=bwBAI/incoming/55725fec1b3e3b629c8929ba0d41fefe'
 //告警灯状态是否点亮
 var isWarnLEDStateOn = false;
 //告警功能是否开启
@@ -25,22 +30,54 @@ var isWarnLEDFuncOn = true;
 rpio.open(LISTENT_PIN, rpio.INPUT, rpio.PULL_DOWN);
 rpio.poll(LISTENT_PIN, pin => {
     // log(':the state of pin ' + pin + ' is '+ rpio.read(pin))
-    
-    //将监听信息发送至控制平台
-	log('send a message to server:' + CONSOLE_URL);
-    CONSOLE_URL && http.get(CONSOLE_URL,(res)=>{
-    	log('and response of this server: ',res);
-    })
-	//只有在告警功能打开时才会闪烁
-    isWarnLEDFuncOn && twinkLED(WARNLED_PIN, 0.5, 2);
-   
 
-//监听上斜波
-},rpio.POLL_HIGH);
+    //将监听信息推送到app
+    log('[rpio->app-hock]request ' + WEB_HOCK);
+
+    var time = new Date().toString().split(' ').splice(1,4).join('-');
+    var cmdStr = `curl '${WEB_HOCK}' \
+      -H 'Content-Type: application/json' \
+      -d '
+    {
+      "text": "[${time}]检测范围有人活动..",
+    }'`;
+    exec(cmdStr, function(err,stdout,stderr){
+            err && log(err);
+            console.log(stdout);
+    });
+
+    // request.post(
+    //     {
+    //         url: 'https://hook.bearychat.com/=bwBAI/incoming/55725fec1b3e3b629c8929ba0d41fefe',
+    //         contentType: 'application/json',
+    //         body:JSON.stringify({"text":"some body have a action now."}),
+    //     },
+    //     function(error, response, body){
+    //         error && log(error)
+    //         console.log(response,'++')
+    //         if(response.statusCode == 200){
+    //             console.log(body);
+    //         }else{
+    //             console.log(response.statusCode);
+    //         }
+    //     }
+    // );
+
+    //将监听信息发送至控制平台
+    log('[rpio->server]request ' + CONSOLE_URL + '/warnSignal');
+    CONSOLE_URL && http.get(CONSOLE_URL+'/warnSignal',(res)=>{
+        log('[server->rpio] ',res);
+    })
+    //只有在告警功能打开时才会闪烁
+    isWarnLEDFuncOn && twinkLED(WARNLED_PIN, 0.5, 2);
+
+
+    //监听上斜波
+}, rpio.POLL_HIGH);
 
 //提供服务
 server({ port: SERVER_PORT }, [
-	//告警灯状态可控
+    //告警灯状态可控
     get('/offWarnLED', ctx => {
         log(' offWarnLED request is coming..');
         offLED(WARNLED_PIN);
@@ -60,12 +97,16 @@ server({ port: SERVER_PORT }, [
 
     //是否开启告警灯功能
     get('/onWarnFunc', ctx => {
-    	isWarnLEDFuncOn = true;
+        isWarnLEDFuncOn = true;
         log(' Warn Functoin is open now..');
     }),
     get('/offWarnFunc', ctx => {
-    	isWarnLEDFuncOn = false
+        isWarnLEDFuncOn = false
         log(' Warn Functoin is close now....');
+    }),
+    get('/changeWarnFunc', ctx => {
+        isWarnLEDFuncOn = isWarnLEDFuncOn ? false : true;
+        log(' Warn Functoin is' + isWarnLEDFuncOn + 'now....');
     }),
 ]);
 
@@ -77,11 +118,11 @@ function twinkLED(pin, sec, n) {
     var sec = sec ? sec : 1;
     //默认闪烁1次
     var n = n ? n : 1
-    for(let i = 0; i < n; i++){
-         if (isWarnLEDStateOn) {
-        rpio.write(pin, rpio.LOW);
-        rpio.sleep(sec);
-        rpio.write(pin, rpio.HIGH);
+    for (let i = 0; i < n; i++) {
+        if (isWarnLEDStateOn) {
+            rpio.write(pin, rpio.LOW);
+            rpio.sleep(sec);
+            rpio.write(pin, rpio.HIGH);
         } else {
             rpio.open(pin, rpio.OUTPUT);
             rpio.write(pin, rpio.HIGH);
@@ -89,10 +130,10 @@ function twinkLED(pin, sec, n) {
             rpio.write(pin, rpio.LOW);
             rpio.close(pin);
         }
-        if(i !== n-1)
+        if (i !== n - 1)
             rpio.sleep(sec);
     }
-   
+
 
     // log(' now the warn LED state: ' + isWarnLEDStateOn)
 }
@@ -119,6 +160,6 @@ function changeLED(pin) {
     isWarnLEDStateOn ? offLED(pin) : onLED(pin);
 }
 
-function log(str){
-	console.log('[' + Date.now() + ']' + str);
+function log(str) {
+    console.log('[' + Date.now() + ']' + str);
 }
